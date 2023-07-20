c42a2d8d8a1dcae0fefce8dffce51da1cf4b1db8
glbrom <golbrm@gmail.com>
2023-07-20 20:30:38 +0300

    Create ScoreAiViewModel

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewModel/ScoreAIViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ScoreViewModel.swift
+//  Tic-Tac-Toe
+//
+//  Created by Macbook on 15.07.2023.
+//
+
+import SwiftUI
+
+final class ScoreViewModel: ObservableObject {
+    
+    static let shared = ScoreViewModel()
+    
+    @Published var wins: Int = 0
+    @Published var losses: Int = 0
+    @Published var draws: Int = 0
+    
+    func increseWinScore() {
+        wins += 1
+    }
+    
+    func increseDrawScore() {
+        draws += 1
+    }
+    
+    func increseLoseScore() {
+        losses += 1
+    }
+    
+    func resetScore() {
+        wins = 0
+        losses = 0
+        draws = 0
+    }
+}
+
